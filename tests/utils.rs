cacc0d8d3ee1f9a7b8cd1e043ed8dc181cacb8b6
Nick Babcock <nbabcock19@hotmail.com>
2020-11-19 16:33:58 -0600

    Simplify test dependencies

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,29 +1,21 @@
-use s3::{creds::Credentials, Bucket, Region};
 use std::path::{Path, PathBuf};
 
 /// Request data from s3 and cache it locally
 pub fn request<S: AsRef<str>>(bucket_name: &str, input: S) -> PathBuf {
     let reffed = input.as_ref();
     let cache = Path::new("assets").join("saves").join(reffed);
-    if cache.exists() {
-        cache
-    } else {
-        let region_name = "us-west-002".to_string();
-        let endpoint = "s3.us-west-002.backblazeb2.com".to_string();
-        let region = Region::Custom {
-            region: region_name,
-            endpoint,
-        };
-        let credentials = Credentials::anonymous().unwrap();
-        let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
-        let (data, code) = bucket.get_object_blocking(reffed).unwrap();
+    if !cache.exists() {
+        let url = format!("https://{}.s3.us-west-002.backblazeb2.com/{}", bucket_name, reffed);
+        let resp = attohttpc::get(&url).send().unwrap();
 
-        if code != 200 {
+        if !resp.is_success() {
             panic!("expected a 200 code from s3");
         } else {
+            let data = resp.bytes().unwrap();
             std::fs::create_dir_all(cache.parent().unwrap()).unwrap();
             std::fs::write(&cache, &data).unwrap();
-            cache
         }
     }
+
+    cache
 }
