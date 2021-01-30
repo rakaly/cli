6194a3d8e77824b63a59b1fdb7d44b7fffa3959b
Nick Babcock <nbabcock19@hotmail.com>
2021-01-30 11:42:57 -0600

    Fix token path on windows CI

--- assets/ci/decrypt_secret.sh
+++ assets/ci/decrypt_secret.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -euo pipefail
 
 cd "$(dirname "$0")"/../..
 for i in "ck3" "eu4" "imperator"; do
-    gpg --quiet --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output assets/tokens/$i.txt assets/tokens/$i.txt.gpg
+    gpg --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output assets/tokens/$i.txt assets/tokens/$i.txt.gpg
 done;
