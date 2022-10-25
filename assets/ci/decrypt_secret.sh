4840019cdee55dd7e6a878ea826f615ab1078bdb
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2022-10-25 20:41:40 +0200

    Clean up build files

--- assets/ci/decrypt_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-cd "$(dirname "$0")"/../..
-for i in "ck3" "eu4" "imperator" "hoi4"; do
-    gpg --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output assets/tokens/$i.txt assets/tokens/$i.txt.gpg
-done;
